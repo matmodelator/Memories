0230689182f992f4b9726abc9cf6fa05d186176e
matmodelator <druzball@tutamail.com>
2026-03-05 01:16:38 +0200

    Update nav-loader.js

--- docs/nav-loader.js
+++ docs/nav-loader.js
@@ -5,5 +5,24 @@ if (slot) {
     .then(r => r.text())
     .then(html => {
       slot.innerHTML = html;
+
+      // определяем текущую страницу
+      const path = window.location.pathname;
+
+      if (path.includes("poems")) {
+        document.querySelector('[data-text="Стихи"]')?.classList.add("nav-active");
+      }
+
+      if (path.includes("prosa")) {
+        document.querySelector('[data-text="Проза"]')?.classList.add("nav-active");
+      }
+
+      if (path.includes("songs")) {
+        document.querySelector('[data-text="Песни"]')?.classList.add("nav-active");
+      }
+
+      if (path.includes("author")) {
+        document.querySelector('[data-text="Автор"]')?.classList.add("nav-active");
+      }
     });
 }
