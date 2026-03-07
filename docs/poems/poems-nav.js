03f954616d5d9de44563e4d3995476511f3e2c24
matmodelator <druzball@tutamail.com>
2026-03-07 17:34:06 +0200

    Create poems-nav.js

---
+++ docs/poems/poems-nav.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", async function(){
+
+  const current = location.pathname.split("/").pop()
+
+  const res = await fetch("../poems.html")
+  const text = await res.text()
+
+  const parser = new DOMParser()
+  const doc = parser.parseFromString(text,"text/html")
+
+  const links = [...doc.querySelectorAll("a[href*='poem']")]
+
+  const files = links.map(a=>a.getAttribute("href"))
+
+  const index = files.indexOf(current)
+
+  const prev = files[index-1]
+  const next = files[index+1]
+
+  let html = '<div class="poem-nav">'
+
+  if(prev)
+    html += `<a href="${prev}">🔼</a>`
+
+  if(next)
+    html += `<a href="${next}">🔽</a>`
+
+  html += '</div>'
+
+  document.getElementById("poem-nav").innerHTML = html
+
+})
