5279374d1c3ee3d217b880852620968f6d21126d
matmodelator <druzball@tutamail.com>
2026-03-14 16:59:00 +0200

    Update poem-nav.js

--- docs/poems/poem-nav.js
+++ docs/poems/poem-nav.js
@@ -1,31 +1,4 @@
- /*document.addEventListener("DOMContentLoaded", function () { 
-  const container = document.getElementById("poem-nav");
-  if (container) {
-    container.innerHTML = `
-      <div class="poem-nav">
-        <a href="#">🔼</a>
-        <a href="#">🔽</a>
-      </div>
-    `;
-  }
-}); */
 
-
-
-/*document.addEventListener("DOMContentLoaded", function () {
-  const container = document.getElementById("poem-nav");
-  if (!container) return;
-
-  const prevPath = "poem_tsc1.html";
-  const nextPath = "poem_tsc2.html";
-
-  container.innerHTML = `
-    <div class="poem-nav">
-      <a href="${prevPath}">🔼</a>
-      <a href="${nextPath}">🔽</a>
-    </div>
-  `;
-});*/
 
 document.addEventListener("DOMContentLoaded", async function () {
 
