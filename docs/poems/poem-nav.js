b7888fe02331feb6b1456baf90eef50ff0b136d8
matmodelator <druzball@tutamail.com>
2026-03-14 17:09:35 +0200

    Update poem-nav.js

--- docs/poems/poem-nav.js
+++ docs/poems/poem-nav.js
@@ -1,5 +1,5 @@
 
-
+/*
 document.addEventListener("DOMContentLoaded", async function () {
 
   const container = document.getElementById("poem-nav");
@@ -28,13 +28,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     if (index !== -1) {
 
-    /* if (files[index - 1]) {
-        prevPath = files[index - 1];
-      }
-
-      if (files[index + 1]) {
-        nextPath = files[index + 1];
-      } */
 if (files[index - 1]) {
   prevPath = files[index - 1].replace(/^poems\//, "");
 }
@@ -54,4 +47,55 @@ if (files[index + 1]) {
   `;
 
 });
+*/
 
+document.addEventListener("DOMContentLoaded", async function () {
+  const container = document.getElementById("poem-nav");
+  if (!container) return;
+
+  // сразу показываем стрелки
+  let prevPath = "#";
+  let nextPath = "#";
+
+  container.innerHTML = `
+    <div class="poem-nav">
+      <a href="${prevPath}">▲</a>
+      <a href="${nextPath}">▼</a>
+    </div>
+  `;
+
+  try {
+    const current = location.pathname.split("/").pop();
+
+    const response = await fetch("/Memories/poems.html");
+    const html = await response.text();
+
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    const links = [...doc.querySelectorAll("a[href$='.html']")];
+    const files = links.map(a => a.getAttribute("href"));
+    const names = files.map(href => href.split("/").pop());
+
+    const index = names.indexOf(current);
+
+    if (index !== -1) {
+      if (files[index - 1]) {
+        prevPath = files[index - 1].replace(/^poems\//, "");
+      }
+
+      if (files[index + 1]) {
+        nextPath = files[index + 1].replace(/^poems\//, "");
+      }
+    }
+
+    container.innerHTML = `
+      <div class="poem-nav">
+        <a href="${prevPath}">▲</a>
+        <a href="${nextPath}">▼</a>
+      </div>
+    `;
+  } catch (e) {
+    // стрелки уже показаны, ничего не делаем
+  }
+});
